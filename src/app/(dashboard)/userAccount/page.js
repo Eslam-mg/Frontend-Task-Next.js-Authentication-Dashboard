d41cc8a459d7db4ee09b2d8b9ae92e49b7cccd2f
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-06 12:07:34 +0200

    add user account page

--- src/app/(dashboard)/userAccount/page.js
+++ src/app/(dashboard)/userAccount/page.js
@@ -1,7 +1,20 @@
+"use client"
 import ProtectedRoute from '@/components/ProtectedRoute/ProtectedRoute'
-import React from 'react'
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react'
 
 export default function page() {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+    const router = useRouter();
+
+    useEffect(() => {
+        
+        return () => {
+            
+        };
+    }, []);
     return (
         <ProtectedRoute>
             <div>userAccount</div>
