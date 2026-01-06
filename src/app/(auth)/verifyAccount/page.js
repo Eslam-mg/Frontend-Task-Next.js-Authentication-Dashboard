6aac0d0c6b41f67337e6c27bd9b5958695e7bbde
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-06 10:40:53 +0200

    Check if user has token, if not redirect to login

--- src/app/(auth)/verifyAccount/page.js
+++ src/app/(auth)/verifyAccount/page.js
@@ -14,6 +14,14 @@ export default function page() {
 
     // Refs to manage focus for each code input box
     const inputRefs = useRef([]);
+    
+    useEffect(() => {
+        // Check if user has token, if not redirect to login
+        const token = authStorage.getToken();
+        if (!token) {
+            router.push('/login');
+        }
+    }, [router]);
     return (
         <div className="min-h-screen bg-linear-to-br from-blue-50 to-indigo-100 flex items-center justify-center p-4">
             <div className="bg-white rounded-2xl shadow-xl p-8 w-full max-w-md">
