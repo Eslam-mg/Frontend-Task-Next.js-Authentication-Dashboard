84245e16e35c0a79e2695c082fac9fd8f2535c3f
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-07 12:36:40 +0200

    add SimilarItems component

---
+++ src/components/SimilarItems/SimilarItems.js
@@ -0,0 +1,47 @@
+'use client';
+import React from 'react';
+
+const products = [
+    {
+        id: 1,
+        title: "J.VER Women's Dress Shirts Solid Long Sleeve",
+        price: 900,
+        oldPrice: 1300,
+        rating: 4.5,
+        reviews: 2910,
+        image: '/products/img1.png',
+        discount: '25% OFF',
+    },
+    {
+        id: 2,
+        title: "J.VER Women's Dress Shirts Solid Long Sleeve",
+        price: 900,
+        rating: 4.5,
+        reviews: 2910,
+        image: '/products/img2.png',
+    },
+    {
+        id: 3,
+        title: "J.VER Women's Dress Shirts Solid Long Sleeve",
+        price: 900,
+        rating: 4.5,
+        reviews: 2910,
+        image: '/products/img3.png',
+        discount: '25% OFF',
+    },
+    {
+        id: 4,
+        title: "J.VER Women's Dress Shirts Solid Long Sleeve",
+        price: 900,
+        rating: 4.5,
+        reviews: 2910,
+        image: '/products/img4.png',
+        discount: '25% OFF',
+    },
+];
+
+export default function SimilarItems() {
+    return (
+        <div>SimilarItems</div>
+    )
+}
